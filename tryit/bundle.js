e2cf438ff64ce311b2930b556530e25ee4f0b825
Mike <17344144+Mike278@users.noreply.github.com>
2025-09-02 22:22:50 -0400

    hovers randomly broke? vendor codemirror and dependencies...

---
+++ tryit/bundle.js
@@ -0,0 +1,7 @@
+// deno bundle --minify bundle.js --output cm.js
+export * as cm from 'https://esm.sh/codemirror';
+export { oneDark } from 'https://esm.sh/@codemirror/theme-one-dark';
+export * as tooltip from 'https://esm.sh/@codemirror/tooltip';
+export * as cmView from 'https://esm.sh/@codemirror/view';
+export * as language from 'https://esm.sh/@codemirror/language';
+export * as lint from 'https://esm.sh/@codemirror/lint';
